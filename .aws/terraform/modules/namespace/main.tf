87c97343e1d5c9bc1bcf4ec0d63197f1c5026b09
Jordan Reed <jordan.reed.aws@gmail.com>
2024-01-29 17:08:37 -0500

    replace slash with dash in branch name for namespace

--- .aws/terraform/modules/namespace/main.tf
+++ .aws/terraform/modules/namespace/main.tf
@@ -2,7 +2,7 @@ locals {
   repo = var.repo
   environment = var.environment
   branch = var.branch
-  namespace = lower("${local.repo}-${local.environment}-${local.branch}")
+  namespace = lower("${local.repo}-${local.environment}-${replace(local.branch, "/", "-")}")
   tags = {
     "repo" : local.repo,
     "environment" : local.environment,
