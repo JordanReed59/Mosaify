84a31c453230358eb105688657984ecb911aadce
Jordan Reed <jordan.reed.aws@gmail.com>
2024-02-01 15:14:48 -0500

    add options method to root of api

--- .aws/terraform/apigateway.tf
+++ .aws/terraform/apigateway.tf
@@ -14,11 +14,11 @@ resource "aws_api_gateway_method" "option_post_method" {
 
 resource "aws_api_gateway_integration" "option_post_lambda" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id = "${aws_api_gateway_method.option_post_method.resource_id}"
+  resource_id = "${aws_api_gateway_method.gateway.root_resource_id}"
   http_method = "${aws_api_gateway_method.option_post_method.http_method}"
 
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  integration_http_method = "${aws_api_gateway_method.option_post_method.http_method}"
+  type                    = "AWS"
   uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${local.account_id}:${module.namespace.namespace}-option/invocations"
 }
 
