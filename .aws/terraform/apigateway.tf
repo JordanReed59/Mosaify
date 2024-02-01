927a8aec71202311f933c661a3dd7e7e35633faf
Jordan Reed <jordan.reed.aws@gmail.com>
2024-01-31 21:24:17 -0500

    update naming

--- .aws/terraform/apigateway.tf
+++ .aws/terraform/apigateway.tf
@@ -35,7 +35,7 @@ resource "aws_lambda_permission" "apigw_auth_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.post_method.http_method}${aws_api_gateway_resource.mosaify_method_resource.path}"
+  source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.auth_post_method.http_method}${aws_api_gateway_resource.mosaify_auth_method_resource.path}"
 }
 ################ Auth resource ################
 
