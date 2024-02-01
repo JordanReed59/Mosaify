31b24deb68f41e866a57498a9131e3f3accd22ba
Jordan Reed <jordan.reed.aws@gmail.com>
2024-01-31 20:14:32 -0500

    deploy api

--- .aws/terraform/apigateway.tf
+++ .aws/terraform/apigateway.tf
@@ -24,5 +24,28 @@ resource "aws_api_gateway_integration" "post_lambda" {
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.mosaify_lambda.invoke_arn}"
+  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:992382358856:function:***-dev-feature-mos-2-auth/invocations"
+}
+
+resource "aws_api_gateway_deployment" "deploy_api" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  depends_on = [
+    aws_api_gateway_resource.mosaify_method_resource,
+    aws_api_gateway_integration.post_lambda,
+    aws_api_gateway_method.post_method
+  ]
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.mosaify_method_resource.id,
+      aws_api_gateway_integration.post_lambda.id,
+      aws_api_gateway_method.post_method.id
+    ]))
+  }
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deploy_api.id
+  rest_api_id   = aws_api_gateway_rest_api.gateway.id
+  stage_name    = "test"
 }
