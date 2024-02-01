42e1f7a31049555c7ee53a6fab05339fff07452c
Jordan Reed <jordan.reed.aws@gmail.com>
2024-01-31 19:08:22 -0500

    add mosaify lambda code

---
+++ src/backend/mosaify/mosaify.py
@@ -0,0 +1,18 @@
+import boto3
+import json
+import os
+
+
+def lambda_handler(event, context):
+    # body = json.loads(event['body'])
+    # key = body['key']
+
+    # url = generate_presigned_url(key)
+    response = {}
+    response['statusCode'] = 200
+    response['headers'] = {
+        'Content-Type': 'application/json'
+    }
+    response['body'] = {'msg' : "Hello from mosaify lambda"}
+
+    return response
