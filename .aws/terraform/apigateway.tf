459ab48d58dd18d1694e4851914244d35cbd876e
Jordan Reed <jordan.reed.aws@gmail.com>
2024-02-01 14:11:32 -0500

    create option method

--- .aws/terraform/apigateway.tf
+++ .aws/terraform/apigateway.tf
@@ -4,6 +4,41 @@ resource "aws_api_gateway_rest_api" "gateway" {
   description = "Terraform Serverless API Gateway for Mosaify"
 }
 
+################ Option resource ################
+resource "aws_api_gateway_resource" "mosaify_option_method_resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  parent_id   = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
+  path_part   = "/"
+}
+
+resource "aws_api_gateway_method" "option_post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.gateway.id
+  resource_id   = aws_api_gateway_resource.mosaify_option_method_resource.id
+  http_method   = "OPTION"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "option_post_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id = "${aws_api_gateway_method.option_post_method.resource_id}"
+  http_method = "${aws_api_gateway_method.option_post_method.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${local.account_id}:function:mosaify-dev-feature-mos-2-option/invocations"
+}
+
+resource "aws_lambda_permission" "apigw_aoption_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "mosaify-dev-feature-mos-2-option"
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.auth_post_method.http_method}${aws_api_gateway_resource.mosaify_auth_method_resource.path}"
+}
+################ Option resource ################
+
 ################ Auth resource ################
 resource "aws_api_gateway_resource" "mosaify_auth_method_resource" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
