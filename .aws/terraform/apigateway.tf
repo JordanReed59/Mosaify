0bf1a9d3c074f840887df5eba4a59baff5c2f2a5
Jordan Reed <jordan.reed.aws@gmail.com>
2024-02-02 09:17:55 -0500

    rm reference to method resource

--- .aws/terraform/apigateway.tf
+++ .aws/terraform/apigateway.tf
@@ -42,7 +42,7 @@ resource "aws_lambda_permission" "apigw_option_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.option_post_method.http_method}${aws_api_gateway_resource.option_method_resource.path}"
+  source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.option_post_method.http_method}/"
 }
 ################ Option resource ################
 
