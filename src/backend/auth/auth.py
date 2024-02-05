4472aa152691a6f55af57d8aee9e264356e5c43a
Jordan Reed <jordan.reed.aws@gmail.com>
2024-02-04 19:05:03 -0500

    implement access token code

--- src/backend/auth/auth.py
+++ src/backend/auth/auth.py
@@ -11,7 +11,6 @@ SECRET_NAME = os.environ.get("SECRET_NAME", "blank")
 REGION = os.environ.get("REGION", "blank")
 TOKEN_URL = os.environ.get("TOKEN_URL", "blank")
 SPOTIFY_CREDENTIALS = {}
-print("Hello from outside handler")
 
 # Obtain spoitify credentials from secrets manager
 def get_secret():
@@ -40,11 +39,14 @@ def get_secret():
             text_secret_data = eval(get_secret_value_response['SecretString'])
             SPOTIFY_CREDENTIALS["CLIENT_ID"] = text_secret_data["CLIENT_ID"]
             SPOTIFY_CREDENTIALS["CLIENT_SECRET"] = text_secret_data["CLIENT_SECRET"]
-            SPOTIFY_CREDENTIALS["REDIRECT_URL"] = text_secret_data["REDIRECT_URL"]
+            SPOTIFY_CREDENTIALS["REDIRECT_URI"] = text_secret_data["REDIRECT_URI"]
         else:
             binary_secret_data = get_secret_value_response['SecretBinary']
 
+        print(f"Successfully retrieved client credentials")
+
 def get_access_token(authorization_code):
+    print(f"Exchanging authorization code for access token")
     token_data = {
         'grant_type': 'authorization_code',
         'code': authorization_code,
@@ -70,10 +72,12 @@ def get_access_token(authorization_code):
         # Extract the access token
         access_token = response_data.get('access_token')
 
+        print("Successfully retrieved token")
         return {
             'statusCode': 200,
             'body': json.dumps({'accessToken': access_token})
         }
+    
     except Exception as e:
         print(f'Error getting access token: {e}')
         return {
@@ -83,6 +87,17 @@ def get_access_token(authorization_code):
 
 def lambda_handler(event, context):
     print(event)
+    print("Retriving Spotify client credentials")
+    get_secret()
+    print(f"Using client id ending with: {SPOTIFY_CREDENTIALS['CLIENT_ID'][0:4]}")
+    print(f"Using client secret ending with: {SPOTIFY_CREDENTIALS['CLIENT_SECRET'][0:4]}")
+    print(f"Using redirect uri: {SPOTIFY_CREDENTIALS['REDIRECT_URI']}")
+
+    authorization_code = event['body']['code']
+    print(f"Authorization Code: {authorization_code}")
+    access_token = get_access_token(authorization_code)
+    print(f"Access Token: {access_token}")
+
     # body = json.loads(event['body'])
     # authorization_code = body['authorization_code']
     # print(authorization_code)
@@ -96,6 +111,6 @@ def lambda_handler(event, context):
         'Content-Type': 'application/json; charset=utf-8',
         'Access-Control-Allow-Origin': origin
     }
-    response['body'] = json.dumps({'msg' : "Hello from auth lambda"})
+    response['body'] = json.dumps({'access_token' : access_token})
 
     return response
