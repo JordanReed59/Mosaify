d507f75c270813719ef061fbe6032a4df3b6d15d
Jordan Reed <jordan.reed.aws@gmail.com>
2024-02-07 23:17:49 -0500

    add explamation point

--- src/frontend/mosaify/src/components/UserInfo.js
+++ src/frontend/mosaify/src/components/UserInfo.js
@@ -23,7 +23,7 @@ const UserInfo = ({ accessToken }) => {
      });
     return (
         <div>
-            <h1> Welcome {userDisplayName}</h1>
+            <h1> Welcome {userDisplayName}!</h1>
             <img src={userImageUrl}/>
         </div>
     );
