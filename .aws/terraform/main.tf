2472e48d8eabc993d61e289bef86c398165d1b9c
Jordan Reed <jordan.reed.aws@gmail.com>
2024-02-12 17:43:15 -0500

    create image buckets

--- .aws/terraform/main.tf
+++ .aws/terraform/main.tf
@@ -4,8 +4,3 @@ module "namespace" {
   environment = var.environment
   branch = var.branch
 }
-
-resource "aws_secretsmanager_secret" "spotify_secret" {
-  name = "${module.namespace.namespace}-SPOTIFY_CLIENT_CREDENTIALS"
-  tags = module.namespace.tags
-}
