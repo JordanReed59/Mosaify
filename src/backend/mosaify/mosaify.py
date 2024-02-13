61c7905adeb89f415cdb8fab57b1cdbf5c8efdec
Jordan Reed <jordan.reed.aws@gmail.com>
2024-02-13 17:13:01 -0500

    refactor img download

--- src/backend/mosaify/mosaify.py
+++ src/backend/mosaify/mosaify.py
@@ -13,16 +13,25 @@ DOWNLOAD_BUCKET_NAME = os.environ.get('DOWNLOAD_BUCKET_NAME')
 def download_image(key):
     try:
         print(f"Downloading {key} from {UPLOAD_BUCKET_NAME} bucket")
-        temp = tempfile.TemporaryFile()
+        # temp = tempfile.TemporaryFile()
         s3_client = boto3.client('s3')
-        s3_client.download_fileobj(UPLOAD_BUCKET_NAME, key, temp)
-        # temp.write(b'Hello world!')
-        temp.seek(0)
+        # s3_client.download_fileobj(UPLOAD_BUCKET_NAME, key, temp)
+        # # temp.write(b'Hello world!')
+        # temp.seek(0)
 
-        # convert tempfile to numpy array using load method
-        imageArr = np.fromfile(temp)
-        # close temp file
-        temp.close()
+        # # convert tempfile to numpy array using load method
+        # imageArr = np.fromfile(temp)
+        # # close temp file
+        # temp.close()
+
+        # Download image file to a temporary file
+        with tempfile.TemporaryFile() as temp:
+            s3_client.download_fileobj(UPLOAD_BUCKET_NAME, key, temp)
+            temp.seek(0)
+            
+            # Read the image data from the temporary file
+            image_data = temp.read()
+            imageArr = np.frombuffer(image_data, dtype=np.uint8)
 
         return imageArr
 
@@ -42,9 +51,9 @@ def lambda_handler(event, context):
     imgArr = download_image(key)
     print(imgArr.shape)
 
-    height = imgArr.shape[0]
-    width = imgArr.shape[1]
-    print(width, height)
+    # height = imgArr.shape[0]
+    # width = imgArr.shape[1]
+    # print(width, height)
     # url = generate_presigned_url(key)
     response = {}
     response['statusCode'] = 200
