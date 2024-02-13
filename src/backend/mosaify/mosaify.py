af7623d4f0d4e7202482755054db93e35682f7bb
Jordan Reed <jordan.reed.aws@gmail.com>
2024-02-13 18:20:02 -0500

    add cv2 to dependencies

--- src/backend/mosaify/mosaify.py
+++ src/backend/mosaify/mosaify.py
@@ -2,6 +2,7 @@ import boto3
 import json
 import os
 import tempfile
+import cv2
 import numpy as np
 
 from botocore.exceptions import ClientError
@@ -14,47 +15,32 @@ DOWNLOAD_BUCKET_NAME = os.environ.get('DOWNLOAD_BUCKET_NAME')
 def download_image(key):
     try:
         print(f"Downloading {key} from {UPLOAD_BUCKET_NAME} bucket")
-        # temp = tempfile.TemporaryFile()
         s3_client = boto3.client('s3')
-        # s3_client.download_fileobj(UPLOAD_BUCKET_NAME, key, temp)
-        # # temp.write(b'Hello world!')
-        # temp.seek(0)
 
-        # # convert tempfile to numpy array using load method
-        # imageArr = np.fromfile(temp)
-        # # close temp file
-        # temp.close()
-
-        # response = s3_client.get_object(Bucket=UPLOAD_BUCKET_NAME, Key=key)
-        # metadata = response['Metadata']
-        # height = int(metadata['height'])
-        # width = int(metadata['width'])
-        # channels = 3
-        # Download image file to a temporary file
         with tempfile.TemporaryFile() as temp:
-            # try:
             s3_client.download_fileobj(UPLOAD_BUCKET_NAME, key, temp)
             temp.seek(0)
 
             img = Image.open(temp)
         
-        # Convert the image to a numpy array
+            # Convert the image to a numpy array
             imageArr = np.array(img)
-                
-                # Read the image data from the temporary file
-                # image_data = temp.read()
-                # imageArr = np.frombuffer(image_data, dtype=np.uint8)
-                # imageArr = imageArr.reshape((height, width, channels))
-                
-            # except Exception as e:
-            #     print(f"ERROR downloading {key} file object from {UPLOAD_BUCKET_NAME} bucket")
-            #     raise e
 
         return imageArr
 
     except Exception as e:
         print(f"ERROR downloading {key} from {UPLOAD_BUCKET_NAME} bucket")
         raise e
+    
+def resize_image(imgArr, scale):
+    print("Printing shape of image array")
+    print(imgArr.shape)
+    width = int(imgArr.shape[1] * scale / 100)
+    height = int(imgArr.shape[0] * scale / 100)
+    dim = (width, height)
+    resizedImage = cv2.resize(imgArr, dim, interpolation = cv2.INTER_AREA)
+    print(resizedImage.shape)
+    return resizedImage
 
 def lambda_handler(event, context):
     print(event)
