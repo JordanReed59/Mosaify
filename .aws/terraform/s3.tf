8c3e65700f1ff9f2717fdd08c0c096401ac02c0d
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-14 11:09:18 -0400

    add cors config to buckets

--- .aws/terraform/s3.tf
+++ .aws/terraform/s3.tf
@@ -1,4 +1,5 @@
 ########## begin s3 upload/download bucket configuration ##########
+# upload bucket
 resource "aws_s3_bucket" "mosaify_upload_bucket" {
   bucket = "${module.namespace.namespace}-image-upload-bucket"
   force_destroy = true
@@ -17,6 +18,18 @@ resource "aws_s3_bucket_lifecycle_configuration" "upload_expiration" {
   }
 }
 
+resource "aws_s3_bucket_cors_configuration" "upload_cors" {
+  bucket = aws_s3_bucket.mosaify_upload_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+}
+
+# download bucket
 resource "aws_s3_bucket" "mosaify_download_bucket" {
   bucket = "${module.namespace.namespace}-image-download-bucket"
   force_destroy = true
@@ -32,6 +45,15 @@ resource "aws_s3_bucket_lifecycle_configuration" "download_expiration" {
       days = 1
     }
     status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_cors_configuration" "download_cors" {
+  bucket = aws_s3_bucket.mosaify_download_bucket.id
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
   }
 }
 ########## end s3 upload/download bucket configuration ##########
