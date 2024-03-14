611fe1b5e53c8c1c5d3923166f91036138ab9c72
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-13 21:40:36 -0400

    update lambda integation path

--- .aws/terraform/apigateway.tf
+++ .aws/terraform/apigateway.tf
@@ -15,69 +15,69 @@ resource "aws_api_gateway_rest_api" "gateway" {
 }
 
 ################ Option resource ################ 
-resource "aws_api_gateway_method" "option_post_method" {
-  rest_api_id   = aws_api_gateway_rest_api.gateway.id
-  resource_id   = aws_api_gateway_rest_api.gateway.root_resource_id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "option_post_method" {
+#   rest_api_id   = aws_api_gateway_rest_api.gateway.id
+#   resource_id   = aws_api_gateway_rest_api.gateway.root_resource_id
+#   http_method   = "OPTIONS"
+#   authorization = "NONE"
+# }
 
-# method response
-resource "aws_api_gateway_method_response" "options_response" {
-  rest_api_id = aws_api_gateway_rest_api.gateway.id
-  resource_id = aws_api_gateway_rest_api.gateway.root_resource_id
-  http_method = aws_api_gateway_method.option_post_method.http_method
-  status_code = 200
-
-  response_models = {
-    "application/json" = "Empty" # Adjust based on your response content type
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-  }
-}
-
-# integration response
-# resource "aws_api_gateway_integration_response" "options_integration_response" {
+# # method response
+# resource "aws_api_gateway_method_response" "options_response" {
 #   rest_api_id = aws_api_gateway_rest_api.gateway.id
 #   resource_id = aws_api_gateway_rest_api.gateway.root_resource_id
 #   http_method = aws_api_gateway_method.option_post_method.http_method
-#   status_code = aws_api_gateway_method_response.options_response.status_code
+#   status_code = 200
 
-#   response_templates = {
-#     "application/json" = ""
+#   response_models = {
+#     "application/json" = "Empty" # Adjust based on your response content type
 #   }
 
 #   response_parameters = {
-#     "method.response.header.Access-Control-Allow-Origin" = "'*'"
-#     "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
-#     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+#     "method.response.header.Access-Control-Allow-Origin" = true
+#     "method.response.header.Access-Control-Allow-Methods" = true
+#     "method.response.header.Access-Control-Allow-Headers" = true
 #   }
 # }
 
+# # integration response
+# # resource "aws_api_gateway_integration_response" "options_integration_response" {
+# #   rest_api_id = aws_api_gateway_rest_api.gateway.id
+# #   resource_id = aws_api_gateway_rest_api.gateway.root_resource_id
+# #   http_method = aws_api_gateway_method.option_post_method.http_method
+# #   status_code = aws_api_gateway_method_response.options_response.status_code
 
-resource "aws_api_gateway_integration" "option_post_lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
-  resource_id = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
-  http_method = "${aws_api_gateway_method.option_post_method.http_method}"
+# #   response_templates = {
+# #     "application/json" = ""
+# #   }
 
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = local.options_lambda_uri
-}
+# #   response_parameters = {
+# #     "method.response.header.Access-Control-Allow-Origin" = "'*'"
+# #     "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,OPTIONS'"
+# #     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type'"
+# #   }
+# # }
 
-resource "aws_lambda_permission" "apigw_option_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = local.options_lambda_name
-  principal     = "apigateway.amazonaws.com"
 
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.option_post_method.http_method}/"
-}
+# resource "aws_api_gateway_integration" "option_post_lambda" {
+#   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+#   resource_id = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
+#   http_method = "${aws_api_gateway_method.option_post_method.http_method}"
+
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = local.options_lambda_uri
+# }
+
+# resource "aws_lambda_permission" "apigw_option_lambda" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = local.options_lambda_name
+#   principal     = "apigateway.amazonaws.com"
+
+#   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+#   source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.option_post_method.http_method}/"
+# }
 ################ Option resource ################
 
 ################ Auth resource ################
