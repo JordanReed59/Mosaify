4b73361cfaab120d965fe49cd746983f78ce50b5
Jordan Reed <jordan.reed.aws@gmail.com>
2024-03-13 21:24:24 -0400

    add options method to url path

--- .aws/terraform/apigateway.tf
+++ .aws/terraform/apigateway.tf
@@ -148,6 +148,53 @@ resource "aws_lambda_permission" "apigw_url_lambda" {
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.url_post_method.http_method}${aws_api_gateway_resource.url_method_resource.path}"
 }
+
+# Url Option resource  
+resource "aws_api_gateway_method" "url_option_post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.gateway.id
+  resource_id   = aws_api_gateway_resource.url_method_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+# method response
+resource "aws_api_gateway_method_response" "url_options_response" {
+  rest_api_id = aws_api_gateway_rest_api.gateway.id
+  resource_id = aws_api_gateway_resource.url_method_resource.id
+  http_method = aws_api_gateway_method.url_option_post_method.http_method
+  status_code = 200
+
+  response_models = {
+    "application/json" = "Empty" # Adjust based on your response content type
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "url_option_post_lambda" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  resource_id = "${aws_api_gateway_resource.url_method_resource.id}"
+  http_method = "${aws_api_gateway_method.url_option_post_method.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = local.options_lambda_uri
+}
+
+resource "aws_lambda_permission" "apigw_url_option_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = local.options_lambda_name
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.region}:${local.account_id}:${aws_api_gateway_rest_api.gateway.id}/*/${aws_api_gateway_method.option_post_method.http_method}/"
+}
+
 ################ URL resource ################
 
 ################ Mosaify resource ################
